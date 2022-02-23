8b42db879ef65de1551c2b003a02d4bb50abb1ca
INGOLD <fogold07@gmail.com>
2022-02-23 13:58:08 +0100

    login et signin opérationnels en connexion simple sans JWT

--- src/main/java/com/keskonmange/restcontrollers/RestControllerUtilisateur.java
+++ src/main/java/com/keskonmange/restcontrollers/RestControllerUtilisateur.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -31,61 +32,62 @@ import com.keskonmange.services.ServiceUtilisateur;
 public class RestControllerUtilisateur {
 
 	private String message;
-	
+
 	@Autowired
 	ServiceUtilisateur su;
-	
-	@Autowired
-    private MessageSource messageSource;
-	
-	@Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	@Autowired
+	private MessageSource messageSource;
+
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	/**
+	 * Méthode qui permet de vérifier si l'utilisateur existe en base de données.
+	 * @param pid
+	 * @throws ErreurUtilisateur
+	 */
 	private void verifUtilisateur(Integer pid) throws ErreurUtilisateur {
-		if(su.findById(pid).isEmpty()){
-			throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.notfound", new Object[]{pid}, Locale.getDefault()));
+		if (su.findById(pid).isEmpty()) {
+			throw new ErreurUtilisateur(
+					messageSource.getMessage("erreur.utilisateur.notfound", new Object[] { pid }, Locale.getDefault()));
 		}
 	}
-		
+
 	private void verifEmail(String email) throws ErreurUtilisateur {
-		if(!su.findByEmail(email).isEmpty() ){
-			throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.email.found", new Object[]{email}, Locale.getDefault()));
+		if (!su.findByEmail(email).isEmpty()) {
+			throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.email.found",
+					new Object[] { email }, Locale.getDefault()));
 		}
 	}
-	
+
 	@GetMapping
-	public Iterable<Utilisateur> getAll(){
+	public Iterable<Utilisateur> getAll() {
 		return su.findAll();
 	}
 
 	@GetMapping("{id}")
-	public Optional<Utilisateur> getOne(@PathVariable("id") Integer pid) throws ErreurUtilisateur{
+	public Optional<Utilisateur> getOne(@PathVariable("id") Integer pid) throws ErreurUtilisateur {
 		verifUtilisateur(pid);
 		return su.findById(pid);
 	}
-	
-	@PostMapping
-	public Utilisateur create(@Valid @RequestBody Utilisateur utilisateur, BindingResult result) throws ErreurUtilisateur{
-		if(result.hasErrors()) {
-			message = "";
-			result.getFieldErrors().forEach(e -> {
-				message += messageSource.getMessage("erreur.datalib", new Object[]{e.getField(), e.getDefaultMessage()}, Locale.getDefault());
-			});
-			throw new ErreurUtilisateur(message);
-		}
-		return su.save(utilisateur);
-	}
 
+
+	/**
+	 * @param user
+	 * @return
+	 * @throws ErreurUtilisateur
+	 */
 	@PostMapping("/signin")
-    public Utilisateur registerUser(@Valid @RequestBody Utilisateur user) throws ErreurUtilisateur{
+	public Utilisateur registerUser(@Valid @RequestBody Utilisateur user) throws ErreurUtilisateur {
 		verifEmail(user.getEmail());
 		user.setRole(Role.USER);
 		user.setPwd(bCryptPasswordEncoder.encode(user.getPwd()));
 		return su.save(user);
-    }	
+	}
 
 	@PostMapping("/login")
-    public Utilisateur loginUser(@RequestBody Utilisateur user) throws ErreurUtilisateur{
+  public Utilisateur loginUser(@RequestBody Utilisateur user) throws ErreurUtilisateur {
 		Utilisateur userReturn = null;
 		
 		if (su.findByEmail(user.getEmail()).isPresent()) {
@@ -96,28 +98,31 @@ public class RestControllerUtilisateur {
 			}
 			else {
 				throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.pwdko", new Object[]{user.getEmail()}, Locale.getDefault()));
+
 			}
 		}
 		else {
 			throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.email.notfound", new Object[]{user.getEmail()}, Locale.getDefault()));
 		}
 		return userReturn;
-    }	
+  }	
 	
-	
+
 	@PutMapping("{id}")
-	public Utilisateur update(@RequestBody Utilisateur utilisateur, @PathVariable ("id") Integer pid) throws ErreurUtilisateur{
+	public Utilisateur update(@RequestBody Utilisateur utilisateur, @PathVariable("id") Integer pid)
+			throws ErreurUtilisateur {
 		verifUtilisateur(pid);
-		if(pid != utilisateur.getId()) {
-			throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.notequals", new Object[]{pid, utilisateur.getId()}, Locale.getDefault()));
+		if (pid != utilisateur.getId()) {
+			throw new ErreurUtilisateur(messageSource.getMessage("erreur.utilisateur.notequals",
+					new Object[] { pid, utilisateur.getId() }, Locale.getDefault()));
 		}
 		return su.save(utilisateur);
 	}
 
 	@DeleteMapping("{id}")
-	public void delete(@PathVariable("id") Integer pid) throws ErreurUtilisateur{
+	public void delete(@PathVariable("id") Integer pid) throws ErreurUtilisateur {
 		verifUtilisateur(pid);
 		// TODO : Vérifier les suppressions des tables relationnelles
 		su.deleteById(pid);
-	}	
+	}
 }
--- src/main/java/com/keskonmange/services/ServiceUtilisateur.java
+++ src/main/java/com/keskonmange/services/ServiceUtilisateur.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Service;
 import com.keskonmange.entities.Utilisateur;
 import com.keskonmange.repository.JpaUtilisateur;
 
+/**
+ * @author fogol
+ *
+ */
 @Service
 public class ServiceUtilisateur {
 
@@ -15,24 +19,46 @@ public class ServiceUtilisateur {
 	JpaUtilisateur ju;
 
 
-
+	/**
+	 * Méthode qui renvoie l'Utilisateur dont l'id est passé en paramètre.
+	 * @param pid
+	 * @return Optional<Utilisateur>
+	 */
 	public Optional<Utilisateur> findById(Integer pid){
 		return ju.findById(pid);
 	}
 
+	/**
+	 * Méthode qui renvoie l'Utilisateur dont l'email est passé en paramètre.
+	 * @param email 
+	 * @return Optional<Utilisateur>
+	 */
 	public Optional<Utilisateur> findByEmail(String email){
 		return ju.findByEmail(email);
 	}
 
 	
+	/**
+	 * Méthode qui renvoie la liste de tous les Utilisaters
+	 * @return Iterable<Utilisateur>
+	 */
 	public Iterable<Utilisateur> findAll(){
 		return ju.findAll();
 	}
 
+	/**
+	 * Méthode qui sauvegarde (create & update) et renvoie l'Utilisateur de la base de données.
+	 * @param utilisateur
+	 * @return Utilisateur
+	 */
 	public Utilisateur save(Utilisateur utilisateur){
 		return ju.save(utilisateur);
 	}
 
+	/**
+	 * Méthoe qui supprime l'Utilisateur de la base de données.
+	 * @param pid as id de l'Utilisateur
+	 */
 	public void deleteById(Integer pid){
 		ju.deleteById(pid);
 	}
