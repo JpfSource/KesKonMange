d3844b7863696a7d0e93e355448b9555e3a4d49d
INGOLD <fogold07@gmail.com>
2022-04-27 21:29:19 +0200

    [Swagger] config pour acces uniquement aux controllers

--- src/main/java/com/keskonmange/ApplicationSwaggerConfig.java
+++ src/main/java/com/keskonmange/ApplicationSwaggerConfig.java
@@ -23,7 +23,7 @@ public class ApplicationSwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.keskonmange")) 
-				.paths(PathSelectors.any())
+				.paths(PathSelectors.ant("/api/**"))
 				.build()
 				.apiInfo(apiDetails());
 	}
